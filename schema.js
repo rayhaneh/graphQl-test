bf292cf096c4cededce3aeaf9eb6dcdeb2907770
Rayhaneh <r.b.assady@gmail.com>
2017-12-03 15:53:12 -0500

    Added and configured the required packages.

---
+++ schema.js
@@ -0,0 +1,40 @@
+const graphQL = require('graphql');
+const _ = require('lodash');
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLSchema,
+} = graphQL;
+
+const users = [
+  {id: '23', firstName: 'Bill', age: 21},
+  {id: '24', firstName: 'Maria', age: 43},
+];
+
+const UserType = new GraphQLObjectType({
+  name: 'User',
+  fields: {
+    id: {type: GraphQLString},
+    firstName: {type: GraphQLString},
+    age: {type: GraphQLInt},
+  }
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    user: {
+      type: UserType,
+      args: {id: {type: GraphQLString}},
+      resolve(parentValue, args) {
+        return _.find(users, {id: args.id});
+      }
+    }
+  }
+});
+
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
+})
---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+
+// Combatibility layer between graphQL and express
+const expressGraphQL = require ('express-graphql');
+const schema = require('./schema');
+
+const app = express();
+
+
+
+app.use('/graphql', expressGraphQL({
+  schema, 
+  graphiql: true,
+}));
+
+
+
+
+
+app.listen(4000, () => {
+  console.log('Demo app is listening on port 4000!');
+})
